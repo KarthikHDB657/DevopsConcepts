98d2ea3b114fa98803bd59fb96a66e5c17b5d487
KarthikHDB657 <karthikbharadwajhd@gmail.com>
2022-01-13 23:47:53 +0530

    new commit

---
+++ terraform/09-ec2-multiple-instances/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  # VERSION IS NOT NEEDED HERE
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  //vpc_id = "vpc-0017ef3c14504be25"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+resource "aws_instance" "http_server" {
+  count =3
+  //ami                    = "ami-0ed9277fb7eb570c9"
+  ami                    = data.aws_ami.aws_linux_2_latest.id
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  //subnet_id              = "subnet-0a7846be4922055fa"
+  subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+  
+}
